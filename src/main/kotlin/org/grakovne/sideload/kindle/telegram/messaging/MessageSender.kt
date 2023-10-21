c34769492d7455d653994a71c731302579dad2d0
grakovne <grakovne@gmail.com>
2023-10-21 14:26:30 +0200

    [WIP] main screen

--- src/main/kotlin/org/grakovne/sideload/kindle/telegram/messaging/MessageSender.kt
+++ src/main/kotlin/org/grakovne/sideload/kindle/telegram/messaging/MessageSender.kt
@@ -3,15 +3,17 @@ package org.grakovne.sideload.kindle.telegram.messaging
 import arrow.core.Either
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.model.Update
+import com.pengrad.telegrambot.model.request.Keyboard
+import com.pengrad.telegrambot.model.request.KeyboardButton
 import com.pengrad.telegrambot.model.request.ParseMode
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup
+import com.pengrad.telegrambot.model.request.ReplyKeyboardRemove
 import com.pengrad.telegrambot.request.SendMessage
 import org.grakovne.sideload.kindle.events.core.EventProcessingError
 import org.grakovne.sideload.kindle.telegram.domain.PreparedMessage
 import org.grakovne.sideload.kindle.telegram.domain.error.UnableSendResponse
 import org.grakovne.sideload.kindle.telegram.localization.MessageType
-import org.springframework.stereotype.Service
 
-@Service
 abstract class MessageSender(private val bot: TelegramBot) {
 
     protected fun sendRawMessage(
@@ -21,6 +23,7 @@ abstract class MessageSender(private val bot: TelegramBot) {
         type: MessageType = MessageType.HTML,
     ): Either<EventProcessingError, Unit> {
         val isMessageSent = SendMessage(chatId, message.text)
+            .replyMarkup(navigation.toReplyKeyboard())
             .setParseMode(type)
             .disableWebPagePreview(message.webPagePreview)
             .let { bot.execute(it).isOk }
@@ -37,6 +40,18 @@ abstract class MessageSender(private val bot: TelegramBot) {
         navigation: List<PreparedMessage>,
         type: MessageType = MessageType.HTML,
     ) = sendRawMessage(origin.message().chat().id().toString(), message, navigation, type)
+}
+
+private fun List<PreparedMessage>.toReplyKeyboard(): Keyboard {
+    if (this.isEmpty()) {
+        return ReplyKeyboardRemove()
+    }
+
+    return ReplyKeyboardMarkup(this.map { it.toButton() }.toTypedArray())
+}
+
+private fun PreparedMessage.toButton(): KeyboardButton {
+    return KeyboardButton(this.text)
 }
 
 private fun SendMessage.setParseMode(type: MessageType): SendMessage = when (type) {
