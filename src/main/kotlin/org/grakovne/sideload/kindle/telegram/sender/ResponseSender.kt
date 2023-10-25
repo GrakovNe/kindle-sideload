1899d7635b465c6ec6b98c845fd20cd4b80d1a95
grakovne <grakovne@gmail.com>
2023-10-25 23:03:50 +0200

    [WIP] stk

--- src/main/kotlin/org/grakovne/sideload/kindle/telegram/sender/ResponseSender.kt
+++ src/main/kotlin/org/grakovne/sideload/kindle/telegram/sender/ResponseSender.kt
@@ -3,14 +3,24 @@ package org.grakovne.sideload.kindle.telegram.sender
 import arrow.core.Either
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.request.SendMessage
+import mu.KotlinLogging
 import org.grakovne.sideload.kindle.telegram.domain.error.UnableSendResponse
 import org.springframework.stereotype.Service
 
 @Service
 class ResponseSender(val bot: TelegramBot) {
 
-    fun sendMessage(message: SendMessage) = when (bot.execute(message).isOk) {
-        true -> Either.Right(Unit)
-        false -> Either.Left(UnableSendResponse)
+    fun sendMessage(message: SendMessage): Either<UnableSendResponse, Unit> {
+        val response = bot.execute(message)
+
+        return when (response.isOk) {
+            true -> Either.Right(Unit)
+            false -> Either.Left(UnableSendResponse)
+                .also { logger.error { "Unable to send the message due to: ${response.description()}" } }
+        }
+    }
+
+    companion object {
+        private val logger = KotlinLogging.logger { }
     }
 }
