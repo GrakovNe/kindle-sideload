3a54512b32d574f1999180abf7b8e563619ad325
grakovne <grakovne@gmail.com>
2023-10-27 18:18:54 +0200

    telegram logger now works with message batches

--- src/main/kotlin/org/grakovne/sideload/kindle/telegram/ConfigurationProperties.kt
+++ src/main/kotlin/org/grakovne/sideload/kindle/telegram/ConfigurationProperties.kt
@@ -3,6 +3,7 @@ package org.grakovne.sideload.kindle.telegram
 import ch.qos.logback.classic.Level
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
 import kotlin.properties.Delegates
 
 @Configuration
@@ -10,4 +11,5 @@ import kotlin.properties.Delegates
 class ConfigurationProperties {
     var token: String by Delegates.notNull()
     var level: Level by Delegates.notNull()
+    var loggingTimeout: Duration = Duration.ofSeconds(15)
 }
--- src/main/kotlin/org/grakovne/sideload/kindle/telegram/logging/AsyncLoggerAppender.kt
+++ src/main/kotlin/org/grakovne/sideload/kindle/telegram/logging/AsyncLoggerAppender.kt
@@ -4,9 +4,12 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.AppenderBase
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.request.SendMessage
+import org.grakovne.sideload.kindle.common.ifTrue
 import org.grakovne.sideload.kindle.telegram.ConfigurationProperties
 import org.grakovne.sideload.kindle.user.reference.service.UserService
 import org.springframework.stereotype.Service
+import java.time.Instant
+import java.util.Collections
 import java.util.concurrent.Executors
 
 @Service
@@ -17,6 +20,9 @@ class AsyncLoggerAppender(
 ) : AppenderBase<ILoggingEvent>() {
 
     private val executor = Executors.newSingleThreadExecutor()
+
+    private val loggingEvents: MutableList<String> = Collections.synchronizedList(mutableListOf<String>())
+    private var nextSendingAfter = Instant.now().plus(configurationProperties.loggingTimeout)
 
     override fun append(event: ILoggingEvent?) {
         if (null == event) {
@@ -29,6 +35,21 @@ class AsyncLoggerAppender(
 
         userService
             .fetchSuperUsers()
-            .forEach { user -> executor.execute { bot.execute(SendMessage(user.id, event.formattedMessage)) } }
+            .forEach { user ->
+                executor.execute {
+                    appendLog(event)
+                    Instant.now().isAfter(nextSendingAfter).ifTrue {
+                        bot.execute(SendMessage(user.id, flush()))
+                        nextSendingAfter = Instant.now().plus(configurationProperties.loggingTimeout)
+                    }
+
+                }
+            }
+    }
+
+    private fun appendLog(event: ILoggingEvent) = loggingEvents.add("${event.level}: ${event.formattedMessage}")
+
+    private fun flush() = synchronized(loggingEvents) {
+        loggingEvents.joinToString("\n").also { loggingEvents.clear() }
     }
 }
