80c1e7998b8f012f4f1767c777f40353a8bb5077
grakovne <grakovne@gmail.com>
2024-07-06 20:16:47 +0200

    able to download file

--- src/main/kotlin/org/grakovne/sideload/kindle/shelf/web/endpoint/ShelfEndpoint.kt
+++ src/main/kotlin/org/grakovne/sideload/kindle/shelf/web/endpoint/ShelfEndpoint.kt
@@ -1,11 +1,17 @@
 package org.grakovne.sideload.kindle.shelf.web.endpoint
 
+import org.grakovne.sideload.kindle.environment.UserEnvironmentService
 import org.grakovne.sideload.kindle.shelf.converter.ShelfContentItemConverter
 import org.grakovne.sideload.kindle.shelf.service.ShelfService
+import org.springframework.core.io.FileSystemResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import java.io.File
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter.ofPattern
@@ -13,15 +19,34 @@ import java.time.format.DateTimeFormatter.ofPattern
 @Controller
 class ShelfEndpoint(
     private val shelfService: ShelfService,
-    private val shelfContentItemConverter: ShelfContentItemConverter
+    private val shelfContentItemConverter: ShelfContentItemConverter,
+    private val environmentService: UserEnvironmentService
 ) {
+
+    @RequestMapping("/download/{environmentId}/{filename}")
+    fun downloadBinary(
+        @PathVariable environmentId: String,
+        @PathVariable filename: String,
+    ): ResponseEntity<FileSystemResource> {
+        val file = environmentService
+            .provideEnvironmentFiles(environmentId)
+            .first { it.name == filename }
+
+        val resource = FileSystemResource(file)
+
+        val headers = HttpHeaders().apply {
+            add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"${file.name}\"")
+            add(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
+        }
+
+        return ResponseEntity(resource, headers, HttpStatus.OK)
+    }
 
     @RequestMapping("/{shortUserId}")
     fun index(
         @PathVariable shortUserId: String,
         model: Model
     ): String {
-
         val files = shelfService
             .fetchShelfContent(shortUserId)
             .map { shelfContentItemConverter.apply(it) }
