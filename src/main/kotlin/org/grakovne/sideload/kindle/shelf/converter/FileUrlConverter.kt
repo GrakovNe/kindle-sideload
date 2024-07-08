74a40076bad810ca826d9be47e2d6fa0e67605fd
grakovne <grakovne@gmail.com>
2024-07-08 20:23:14 +0200

    fix url compatibility

--- src/main/kotlin/org/grakovne/sideload/kindle/shelf/converter/FileUrlConverter.kt
+++ src/main/kotlin/org/grakovne/sideload/kindle/shelf/converter/FileUrlConverter.kt
@@ -1,12 +1,9 @@
 package org.grakovne.sideload.kindle.shelf.converter
 
 import com.ibm.icu.text.Transliterator.getInstance
-import org.apache.commons.text.StringEscapeUtils
 
 private val transliterator = getInstance("Any-Latin; NFD; [:Nonspacing Mark:] Remove; NFC")
 
 fun String.toFileName() = transliterator
     .transliterate(this)
-    .let { StringEscapeUtils.escapeHtml4(it) }
-    .replace(" ", "_")
-    .filter { it.code in 32..126 }
+    .replace(Regex("[^a-zA-Z0-9]"), "_")
